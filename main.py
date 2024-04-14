8be1170e8e2449ac0ab672ab243ff1c06ac8480e
alina <Akhmedzhanova.A@yandex.ru>
2024-04-14 22:47:37 +0400

    я получил сообщение /start

---
+++ main.py
@@ -0,0 +1,40 @@
+import logging
+from telegram.ext import Application, MessageHandler, filters
+
+BOT_TOKEN = '6533161431:AAH_iqGGFB5Ulk75XwXgdwa4zM16e5ccYJs'
+# @eholyceum_bot
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def echo(update, context):
+    await update.message.reply_text(f'Я получил сообщение {update.message.text}')
+
+
+def main():
+    # Создаём объект Application.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    # Создаём обработчик сообщений типа filters.TEXT
+    # из описанной выше асинхронной функции echo()
+    # После регистрации обработчика в приложении
+    # эта асинхронная функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(filters.TEXT, echo)
+
+    # Регистрируем обработчик в приложении.
+    application.add_handler(text_handler)
+
+    # Запускаем приложение.
+    application.run_polling()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
