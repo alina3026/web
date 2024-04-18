ab0e566ebabb7d7f7a9250b701542868908b83d0
alina <Akhmedzhanova.A@yandex.ru>
2024-04-18 20:31:06 +0400

    вопросы

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3
 import logging
 from telegram.ext import Application, MessageHandler, filters
 from telegram.ext import CommandHandler
@@ -27,8 +28,9 @@ async def start(update, context):
         "Привет! Это бот квизов. Чтобы начать новую игру введи ник.",
         reply_markup=ReplyKeyboardRemove()
     )
-    global k
-    k = 1
+    global user_in
+    user_in = 1
+
 
 async def close_keyboard(update, context):
     await update.message.reply_text(
@@ -38,25 +40,50 @@ async def close_keyboard(update, context):
 
 
 async def echo(update, context):
-    global k
-    if k == 1:
-        await update.message.reply_text(f'Пользователь {update.message.text} зарегистирован!')
-        k = 0
+    global user_in
+    if user_in == 1:
+        await update.message.reply_text(
+            f'Пользователь {update.message.text} зарегистирован! Нажми "/game", чтобы начать игру')
+        user_in = 0
+
+        # connection = sqlite3.connect('tg_bot.sqlite')
+        # cursor = connection.cursor()
+        # result_id = cursor.execute("""SELECT id FROM users ORDER BY id DESC limit 1""").fetchall()
+        # id = result_id[0][0]
+        # cursor.execute(f"INSERT INTO users VALUES (?,?,?), (id + 1, {update.message.text}, '')")
+        # connection.commit()
+        # connection.close()
+
     else:
         await update.message.reply_text("Выбери ответ из предложенных!")
+
 
 async def help(update, context):
     await update.message.reply_text(
         "Я бот справочник.")
 
 
-async def address(update, context):
-    await update.message.reply_text(
-        "Адрес: г. Москва, ул. Льва Толстого, 16")
+async def game(update, context):
+    global user_in
+    points = 2
+    # await update.message.reply_text(
+    #     "Адрес: г. Москва, ул. Льва Толстого, 16")
+    while points != 0:
+        connection = sqlite3.connect('tg_bot.sqlite')
+        cursor = connection.cursor()
+        result = cursor.execute(
+            """SELECT ques, ans1, ans2, ans3, ans4, true_ans FROM questions ORDER BY RANDOM() limit 1""").fetchall()
 
-
-async def phone(update, context):
-    await update.message.reply_text("Телефон: +7(495)776-3030")
+        connection.commit()
+        connection.close()
+        reply_keyboard = [[f'{result[0][1]}'], [f'{result[0][2]}'],
+                          [f'{result[0][3]}'], [f'{result[0][4]}']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        await update.message.reply_text(
+            result[0][0],
+            reply_markup=markup
+        )
+        break
 
 
 async def site(update, context):
@@ -64,9 +91,7 @@ async def site(update, context):
         "Сайт: http://www.yandex.ru/company")
 
 
-async def work_time(update, context):
-    await update.message.reply_text(
-        "Время работы: круглосуточно.")
+
 
 
 def main():
@@ -74,16 +99,13 @@ def main():
     # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
     application = Application.builder().token(BOT_TOKEN).build()
 
-
     # Зарегистрируем их в приложении перед
     # регистрацией обработчика текстовых сообщений.
     # Первым параметром конструктора CommandHandler я
     # вляется название команды.
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("address", address))
-    application.add_handler(CommandHandler("phone", phone))
+    application.add_handler(CommandHandler("game", game))
     application.add_handler(CommandHandler("site", site))
-    application.add_handler(CommandHandler("work_time", work_time))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("close", close_keyboard))
 
@@ -92,10 +114,10 @@ def main():
     # После регистрации обработчика в приложении
     # эта асинхронная функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
-
-    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    # Регистрируем обработчик в приложении.
-    application.add_handler(text_handler)
+    if user_in == 1:
+        text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+        # Регистрируем обработчик в приложении.
+        application.add_handler(text_handler)
 
     # Запускаем приложение.
     application.run_polling()
