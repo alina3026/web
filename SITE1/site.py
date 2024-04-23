e2e78a872dd26007d277fda1aa45227a73646144
west1 <sotnikova2308@yandex.ru>
2024-04-23 18:11:22 +0400

    работает сайт

--- SITE1/site.py
+++ SITE1/site.py
@@ -4,8 +4,22 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+@app.route('/Главная.html')
+@app.route('/templates/Главная.html')
+def gl():
     return render_template('index.html')
+
+
+@app.route('/templates/Телеграм-бот.html')
+@app.route('/Телеграм-бот.html')
+def bot():
+    return render_template('Телеграм-бот.html')
+
+
+@app.route('/templates/Таблица-рейтинга.html')
+@app.route('/Таблица-рейтинга.html')
+def bot456():
+    return render_template('Таблица-рейтинга.html')
 
 
 if __name__ == '__main__':
